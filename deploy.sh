2c43c1781824b8f020f44349dffc500a46fd7b82
jpalmasolutions <jpalmasolutions@gmail.com>
2023-04-20 10:28:00 -0400

    using pipenv to run lint, adding comments

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -exu
-flake8 src/
 
+#Install Venv and make sure linting is clean
+pipenv install --dev
+pipenv run flake8 src/
+
+# Get SSO token before running AWS Commands
 aws sso login
 
 sam build
 sam local invoke --event local/json/local-event.json
 
+## Inspect local invoke and make sure the lambda passed.
+echo
 read -p "Continue Script? [N]" -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -14,4 +20,5 @@ then
     exit 1
 fi
 
+# Deploy if y/Y is entered
 sam deploy --guided
