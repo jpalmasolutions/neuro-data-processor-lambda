9186e743b50364be68bb9a8e9f4e5e2e7316309a
jpalmasolutions <jpalmasolutions@gmail.com>
2023-04-28 08:54:54 -0400

    Adding more from Matlab script

--- src/main/processor.py
+++ src/main/processor.py
@@ -18,6 +18,7 @@ def process_block(data: StructType):
 
     sampling_rate: numpy.float64 = block_data.streams.lfpx.fs
     lfp_data_stream: numpy.ndarray = block_data.streams.lfpx.data
+    channels: list[int] = block_data.streams.lfpx.channels
     trial_start_times: numpy.ndarray = block_data.epocs.get("SPAT").onset
 
     inverse_sampling = 1 / sampling_rate
@@ -49,9 +50,16 @@ def process_block(data: StructType):
     trial_x_axis = numpy.arange(start=0.0, step=inverse_sampling, stop=trial_size)
 
     number_of_trials = len(trial_start_times)
-    truncate_start = True
-    trial_ticks = round(trial_size * round(sampling_rate))
-    average_channel_data = numpy.zeros(16, trial_ticks)
+    truncate_start = 0
+    trial_ticks: int = int(round(trial_size * round(sampling_rate)))
+
+    logger.info(
+        msg="trial_ticks",
+        trial_ticks=trial_ticks,
+        trial_ticks_type=trial_ticks.__class__,
+    )
+
+    average_channel_data: numpy.ndarray = numpy.zeros([16, trial_ticks])
 
     logger.info(
         msg="Data",
@@ -67,3 +75,43 @@ def process_block(data: StructType):
         truncate_start=truncate_start,
         average_channel_data=average_channel_data,
     )
+
+    temp_snippet_start = numpy.argmax(a=(x_axis >= trial_start_times[0]))
+    temp_snippet_end = temp_snippet_start + trial_ticks
+
+    for channel in channels:
+        offset_channel: int = channel - 1
+
+        temp_channel_data: numpy.ndarray = numpy.zeros(
+            [number_of_trials, trial_ticks + 1]
+        )
+
+        for trial in range(truncate_start, number_of_trials + truncate_start - 1):
+            if (trial_start_times[trial] + trial_size) < exp_duration:
+                temp_channel_data[trial] = lfp_data_stream[offset_channel][
+                    temp_snippet_start : temp_snippet_end + 1
+                ]
+
+                if trial == truncate_start:
+                    experiment_start = numpy.argmax(
+                        a=(x_axis >= trial_start_times[trial])
+                    )
+
+                    logger.info(
+                        msg="Experiment Start Set.", experiment_start=experiment_start
+                    )
+
+                if trial == number_of_trials + truncate_start - 1:
+                    experiment_end = (
+                        numpy.argmax(a=(x_axis >= trial_start_times[trial]))
+                        + trial_ticks
+                    )
+
+                    logger.info(
+                        msg="Experiment End Set.", experiment_end=experiment_end
+                    )
+
+        temp_channel_data = temp_channel_data[truncate_start:]
+        average_channel_data[offset_channel] = numpy.mean(temp_channel_data)
+
+    logger.info(msg="Channel Data", average_channel_data=average_channel_data)
